27c88f7c12b9834fa8f16c280316cc2dbf46b670
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-09-11 14:38:47 -0300

    feat: solução do problema 2861 do beecrowd

---
+++ src/Iniciante/Iniciante2861.java
@@ -0,0 +1,25 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+public class Iniciante2861 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int C = sc.nextInt();
+
+        for (int i = 0; i < C; i++) {
+
+            String pergunta = sc.nextLine();
+
+            System.out.println("gzuz");
+
+        }
+
+        sc.close();
+
+    }
+
+}
