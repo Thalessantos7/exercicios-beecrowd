bc06fe273c347889a80dc2c38207b1a676fa60a4
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-09-26 12:51:59 -0300

    feat(sql): adiciona solução do exercício 2622

---
+++ sql/2622.sql
@@ -0,0 +1,8 @@
+SELECT
+    customers.name
+FROM
+    customers
+INNER JOIN
+        legal_person ON legal_person.id_customers = customers.id
+WHERE
+    customers.id IN (SELECT id_customers FROM legal_person)
